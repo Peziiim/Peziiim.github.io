0826ddbe7195d5abf4cbfc1e707bd37c9e7f1d7c
peziiim <eduardoorx@gmail.com>
2025-03-13 17:19:48 -0300

    feat: added copy to click feature

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ const sliders = document.querySelectorAll('.slider');
 const sliders2 = document.querySelectorAll('.slider2');
 const btnPrev = document.getElementById('prev-button');
 const btnNext = document.getElementById('next-button');
+const button = document.querySelector('.click-to-copy');
 
 let currentSlide = 0;
 
@@ -65,3 +66,40 @@ function prevSlider2() {
 
 btnPrev.addEventListener('click', prevSlider);
 btnNext.addEventListener('click', nextSlider);
+
+
+
+
+
+
+
+
+const clickToCopy = (e) => {
+  e.preventDefault();
+  copyToClipboard(e.currentTarget.textContent);
+  e.target.classList.add('is-copied');
+  setTimeout(() => { e.target.classList.remove('is-copied') }, 1200);
+}
+
+
+const copyToClipboard = (str) => {
+  const el = document.createElement('textarea');
+  el.value = str;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  const selected =
+    document.getSelection().rangeCount > 0 
+      ? document.getSelection().getRangeAt(0)
+      : false;
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  if (selected) {
+    document.getSelection().removeAllRanges();
+    document.getSelection().addRange(selected);
+  }
+}
+
+button.addEventListener('click', clickToCopy);
